0c8df9419ec844a47a0cffc22462c53a6e3bb775
Gaurav Saini <gauravsaini072005@gmail.com>
2026-04-03 17:44:17 +0530

    test userRoutes only

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const { connectToMongoDB } = require("./connection");
 const { clerkMiddleware } = require('@clerk/express');
 const { clerkwebHooks } = require('./controllers/clerk');
 const userRouter = require("./routes/userRoutes.js");
-const projectRouter = require("./routes/projectRoutes.js");
+// const projectRouter = require("./routes/projectRoutes.js");
 const app = express();
 
 app.get("/api/test", (req, res) => {
