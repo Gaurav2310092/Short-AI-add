3d68f95cfe188d0caec6b861a570ccfad89b0040
Gaurav Saini <gauravsaini072005@gmail.com>
2026-04-03 17:43:11 +0530

    test imports step 4

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const { connectToMongoDB } = require("./connection");
 const { clerkMiddleware } = require('@clerk/express');
 const { clerkwebHooks } = require('./controllers/clerk');
+const userRouter = require("./routes/userRoutes.js");
+const projectRouter = require("./routes/projectRoutes.js");
 const app = express();
 
 app.get("/api/test", (req, res) => {
