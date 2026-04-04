512c43bfff9086769f4677ebd39efdc4deaf2cc1
Gaurav Saini <gauravsaini072005@gmail.com>
2026-04-04 15:52:01 +0530

    updated changes

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,22 @@ connectToMongoDB(process.env.MONGODB_URI);
 
 //Middleware
 app.use(cors({
-    origin:[process.env.CLIENT_URL,"https://short-ai-add-tycr.vercel.app/"],
-    credentials:true,
-    methods:["GET","POST","PUT","DELETE"],
-    allowedHeaders:["Content-Type","Authorization"]
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "https://short-add.vercel.app", // 👈 your frontend
+    ];
+
+    // allow requests with no origin (like Clerk webhooks / curl)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
 }));
 
 app.post('/api/clerk',express.raw({ type: 'application/json' }),clerkwebHooks)
